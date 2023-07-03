1e63c3eebfa7ef6232f679c6452686bea38c32f8
jjxmonster <kuba.tarabasz@wp.pl>
2023-07-03 23:41:54 +0200

    Button text

--- src/app/form-client-component.tsx
+++ src/app/form-client-component.tsx
@@ -36,7 +36,7 @@ const FormClientComponent = () => {
             type="submit"
             className="text-white mt-5 bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
           >
-            Dark
+            Send Message
           </button>
         </div>
       </form>
